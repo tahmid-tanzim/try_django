dba723e9ad06c6b35d6701fd5bcbd4d57e9d3dc7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-02-07 13:56:33 +0600

    Views and Generic Views

--- pollsapi/pollsapi/urls.py
+++ pollsapi/pollsapi/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include('polls.urls')),
+    path('polls/', include('polls.urls')),
 ]
