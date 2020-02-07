592eca7f647facf3f105e718456fd7e1ca2d72ae
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-02-07 17:53:23 +0600

    A better URL structure

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -12,6 +12,6 @@ from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 urlpatterns = [
     path("", PollList.as_view(), name="polls_list"),
     path("<int:pk>/", PollDetail.as_view(), name="polls_detail"),
-    path("choices/", ChoiceList.as_view(), name="choice_list"),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
+    path("<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
 ]
