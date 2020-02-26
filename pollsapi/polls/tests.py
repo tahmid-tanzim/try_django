dedc46c24256629dc00b00e9f1566a8bd3af4aa7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-02-26 14:30:46 +0600

    8.3 Using APIClient

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -2,13 +2,14 @@
 
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.test import APIClient, APITestCase, APIRequestFactory
 from . import apiviews
 
 
 class TestPoll(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.client = APIClient()
 
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
@@ -20,13 +21,21 @@ class TestPoll(APITestCase):
     @staticmethod
     def setup_user():
         User = get_user_model()
-        return User.objects.create_user('test', email='testuser@test.com', password='test')
+        return User.objects.create_user('test', email='testuser@test.com', password='test101')
 
     def test_list(self):
         request = self.factory.get(self.uri,
                                    HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
         request.user = self.user
         response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test101")
+        response = self.client.get(self.uri)
+
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
