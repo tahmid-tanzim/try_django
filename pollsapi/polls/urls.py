ec25cd81e5afe5c36ce192066bc7eba5c96731ad
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-02-26 14:54:27 +0600

    Adding swagger & coreapi documentation

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
 from .apiviews import UserCreate, LoginView, PollViewSet, PollList, PollDetail, ChoiceList, CreateVote
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
+
+schema_view = get_swagger_view(title='Polls API')
 
 urlpatterns = [
     path("users/", UserCreate.as_view(), name="user_create"),
@@ -15,6 +20,8 @@ urlpatterns = [
     # path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    url(r'swagger-docs/', schema_view),
+    path(r'docs/', include_docs_urls(title='Polls API')),
 ]
 
 urlpatterns += router.urls
--- pollsapi/pollsapi/settings.py
+++ pollsapi/pollsapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'polls',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
