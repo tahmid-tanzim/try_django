8d04789d9e57699bbe5ad262fb890c466b8f97c0
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-02-26 14:16:15 +0600

    8.1 Creating Test Requests

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ So which one should you use when? My rule of thumb is,
 * Use viewsets.ModelViewSet when you are going to allow all or most of CRUD operations on a model.
 * Use generics.* when you only want to allow some operations on a model
 * Use APIView when you want to completely customize the behaviour.
+
+
+### Testing and Continuous Integeration
+* APIRequestFactory: This is similar to Django’s RequestFactory. It allows you to create requests
+with any http method, which you can then pass on to any view method and compare responses.
+* APIClient: similar to Django’s Client. You can GET or POST a URL, and test responses.
+* APITestCase: similar to Django’s TestCase. Most of your tests will subclass this.
--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from . import apiviews
+
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
