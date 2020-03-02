16643252735e2c41d836fccd4727c9071dfc5a4e
opangkteyo <opangkteyo@gmail.com>
2020-03-02 16:28:19 +0600

    add enforcer

--- pollsapi/user_profile/views.py
+++ pollsapi/user_profile/views.py
@@ -3,27 +3,28 @@ from django.views import View
 from django.core.files import File
 import casbin
 
+import os
+from django.conf import settings
+
 
 class Home(View):
-    # e = casbin.Enforcer("config/model.conf", "config/policy.csv")
+    model_file = os.path.join(settings.BASE_DIR, 'user_profile/config/model.conf')
+    policy_file = os.path.join(settings.BASE_DIR, 'user_profile/config/policy.csv')
+    e = casbin.Enforcer(model_file, policy_file)
 
     def get(self, request, *args, **kwargs):
 
-        with open('config/policy.csv', 'r') as f:
-            data = File(f)
-            print(data.read)
-            # return JsonResponse(dict(data))
-            return JsonResponse({'id': 420})
+        #return JsonResponse({'id': 420})
 
-        # sub = "alice"  # the user that wants to access a resource.
-        # obj = "address_data"  # the resource that is going to be accessed.
-        # act = "read"  # the operation that the user performs on the resource.
-        #
-        # if self.e.enforce(sub, obj, act):
-        #     roles = self.e.get_roles_for_user(sub)
-        #     print("Roles for {} - ".format(sub), roles)
-        #     return JsonResponse({'message': 'Hello, World!'}, status=200)
-        # else:
-        #     return JsonResponse({'message': "Sorry, {} cannot {} {}".format(sub, act, obj)}, status=404)
+        sub = "tanzim"  # the user that wants to access a resource.
+        obj = "crime_data"  # the resource that is going to be accessed.
+        act = "write"  # the operation that the user performs on the resource.
+
+        if self.e.enforce(sub, obj, act):
+            roles = self.e.get_roles_for_user(sub)
+            print("Roles for {} - ".format(sub), roles)
+            return JsonResponse({'message': 'Hello, World!'}, status=200)
+        else:
+            return JsonResponse({'message': "Sorry, {} cannot {} {}".format(sub, act, obj)}, status=404)
 
 
